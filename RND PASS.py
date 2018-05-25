246bf49450f55df7efbe0775dcc78fed2b32834a
GoluzinaD <goluzinadasha@gmail.com>
2018-05-25 18:07:30 +0300

    KURSOVAYA

---
+++ RND PASS.py
@@ -0,0 +1,10 @@
+import random
+# короткий вариант: print(''.join([random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')) for x in range(12)]))
+str1 = '0123456789'
+str2 = 'qwertyuiopasdfghjklzxcvbnm'
+str3 = str2.upper()
+str4 = str1+str2+str3
+ls = list(str4)
+random.shuffle(ls)
+psw = ''.join([random.choice(ls) for x in range(12)])
+print(psw)
