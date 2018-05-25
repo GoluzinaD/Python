d00fb669093c6aae58094d1f0542c4b16db9afa2
GoluzinaD <goluzinadasha@gmail.com>
2018-05-25 19:19:38 +0300

    Update RND PASS.py

--- RND PASS.py
+++ RND PASS.py
@@ -1,5 +1,5 @@
 import random
-# короткий вариант: print(''.join([random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')) for x in range(12)]))
+#короткий вариант: psw=(''.join([random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')) for x in range(12)]))
 str1 = '0123456789'
 str2 = 'qwertyuiopasdfghjklzxcvbnm'
 str3 = str2.upper()
@@ -7,4 +7,9 @@ str4 = str1+str2+str3
 ls = list(str4)
 random.shuffle(ls)
 psw = ''.join([random.choice(ls) for x in range(12)])
-print(psw)
+file = open("file.txt", "w")
+file.write(psw)
+file.close()
+#print(psw)
+
+
